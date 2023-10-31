66764d1f1ca07442fe3de5c5ec7eeb895b576a03
fukao <c0b2212566@edu.teu.ac.jp>
2023-10-31 16:49:22 +0900

    初期状態

---
+++ bombwake.py
@@ -0,0 +1,39 @@
+import pygame
+import random
+import math
+
+# Pygameの初期化
+pygame.init()
+
+# 画面の設定
+screen_width = 768
+screen_height = 575
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("ボムへいをわけろ！")
+
+# 背景画像の読み込み
+background = pygame.image.load("ex05/deta/background.png")
+
+# ボムの設定
+bomb_image = pygame.image.load("ex05/deta/bom1.png")
+bomb_image = pygame.transform.rotozoom(bomb_image, 0, 0.05)
+bomb_rect = bomb_image.get_rect()
+bomb_spawn_interval = 3000  # ボムの出現間隔（ミリ秒）
+next_bomb_spawn_time = 0
+
+
+# ゲームループ
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    # 背景画像の描画
+    screen.blit(background, (0, 0))
+
+    # 画面更新
+    pygame.display.update()
+
+# Pygameの終了
+pygame.quit()
