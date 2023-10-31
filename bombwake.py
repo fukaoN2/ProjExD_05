7d27d15ee18ede531a54b598a6b1e4071b93243b
fukao <c0b2212566@edu.teu.ac.jp>
2023-10-31 17:21:14 +0900

    ボムの表示、移動および壁反射

--- bombwake.py
+++ bombwake.py
@@ -21,6 +21,24 @@ bomb_rect = bomb_image.get_rect()
 bomb_spawn_interval = 3000  # ボムの出現間隔（ミリ秒）
 next_bomb_spawn_time = 0
 
+bombs = []
+
+# ボムの位置と速度
+bomb_x = screen_width // 2 - bomb_rect.width // 2  # 画面の中央部分に配置
+bomb_y = 0
+while(True):
+    c = random.randint(-1, 1)
+    if not(c == 0):
+        bomb_speed_x = c
+        break
+while(True):
+    b = random.random()
+    if not(b == 0):
+        bomb_speed_y = b
+        break
+a = math.sqrt(abs(1/(bomb_speed_x**2)+(bomb_speed_y**2)))
+bomb_speed_x = bomb_speed_x * a
+bomb_speed_y = bomb_speed_y * a
 
 # ゲームループ
 running = True
@@ -28,9 +46,27 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
+
     # 背景画像の描画
     screen.blit(background, (0, 0))
+
+    # ボムの描画
+    screen.blit(bomb_image, (bomb_x, bomb_y))
+
+    # ボムの位置更新
+    bomb_x += (bomb_speed_x * 0.5)
+    bomb_y += (bomb_speed_y * 0.5)
+
+    # ボムが壁に当たった場合の反射
+    if bomb_x < 0 or bomb_x > screen_width - bomb_rect.width:
+        bomb_speed_x *= -1
+    if bomb_y < 0 or bomb_y > screen_height - bomb_rect.height:
+        bomb_speed_y *= -1
+
+    # ボムの位置更新
+    bomb_x += (bomb_speed_x * 0.5)
+    bomb_y += (bomb_speed_y * 0.5)
+
 
     # 画面更新
     pygame.display.update()
