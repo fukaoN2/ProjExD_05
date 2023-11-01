4b8d7eb1b620f6eed8d0dea227fec362c8016e43
fukao <c0b2212566@edu.teu.ac.jp>
2023-11-01 14:34:36 +0900

    反射範囲指定仮

--- bombwake.py
+++ bombwake.py
@@ -25,6 +25,11 @@ bomb_rect = bomb_image.get_rect()
 bomb_spawn_interval = 3000  # ボムの出現間隔(ミリ秒)
 next_bomb_spawn_time = 0
 
+#格子の追加
+black_floor = pygame.image.load("ex05/data/black.png")
+red_floor = pygame.image.load("ex05/data/red.png")
+yellow_floor = pygame.image.load("ex05/data/yellow_lines.jpg")
+
 # ボムの位置と速度
 bomb_x = screen_width // 2 - bomb_rect.width // 2  # 画面の中央部分に配置
 bomb_y = 0
@@ -55,19 +60,63 @@ def bomb_mvdef():
     new_bomb_y = bomb_y + mv_y
 
     # 壁との衝突チェック
-    if new_bomb_x < 0 or new_bomb_x > screen_width - bomb_rect.width:
+    if new_bomb_x < 24 or new_bomb_x > 774 - bomb_rect.width:
         bomb_speed_x *= -1
     else:
         bomb_x = new_bomb_x
 
-    if new_bomb_y < 0 or new_bomb_y > screen_height - bomb_rect.height:
+    if (new_bomb_y < 76 and bomb_speed_y < 0) or new_bomb_y > 574 - bomb_rect.height:
         bomb_speed_y *= -1
     else:
         bomb_y = new_bomb_y
 
+    # 赤側上辺
+    if 24 <= bomb_x <= 194 and 110 <= bomb_y <= 170 and bomb_speed_y > 0:
+        bomb_speed_y *= -1
+
+    # 黒側上辺
+    if 555 <= bomb_x <= 744 and 110 <= bomb_y <= 170 and bomb_speed_y > 0:
+        bomb_speed_y *= -1
+
+    # 赤側底辺
+    if 24 <= bomb_x <= 194 and 390<= bomb_y <= 410 and bomb_speed_y < 0:
+        bomb_speed_y *= -1
+
+    # 黒側底辺
+    if 555 <= bomb_x <= 744 and 390<= bomb_y <= 410 and bomb_speed_y < 0:
+        bomb_speed_y *= -1
+
+    # 赤側右辺
+    if 194 <= bomb_x <= 214 and 170 <= bomb_y <= 390 and bomb_speed_x < 0:
+        bomb_speed_x *= -1
+
+    # 黒側左辺
+    if 543 <= bomb_x <= 563 and 170 <= bomb_y <= 390 and bomb_speed_x > 0:
+        bomb_speed_x *= -1
+
+
     # ボムの描画
     screen.blit(bomb_image, (bomb_x, bomb_y))
 
+def safezone_def():
+    # # 外枠の描画(実装時に削除・反射を確認するために描画)
+    # pygame.draw.rect(background,(0,0,255),(24,76,750,498))
+
+    # # 安全地帯の黄色の枠の範囲
+    # pygame.draw.rect(background,(255,241,0),(555,170,189,240))
+    # pygame.draw.rect(background,(255,241,0),(24,170,190,240))
+
+    # 安全地帯の注意色の描画
+    screen.blit(yellow_floor, (583, 180))
+    screen.blit(yellow_floor, (24, 180))
+
+    # # 安全地帯の黒と赤の四角の範囲
+    # pygame.draw.rect(background,(0,0,0),(575,190,170,200))
+    # pygame.draw.rect(background,(255,0,0),(24,190,169,200))
+
+    # 安全地帯の黒と赤の格子の描画
+    screen.blit(black_floor, (604, 204))
+    screen.blit(red_floor, (24, -19))
 
 bombs = []
 
@@ -98,7 +147,11 @@ while running:
 
     back()
 
+    safezone_def()
+
     bomb_mvdef()
+
+
 
     # 画面更新
     pygame.display.update()
